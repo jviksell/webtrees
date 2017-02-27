93b97533f5f59e69cec22ff429fb6d9b6c593f80
Greg Roach <fisharebest@gmail.com>
2017-02-27 21:47:55 +0000

    wip

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -30,7 +30,7 @@ global $WT_TREE, $SEARCH_SPIDER;
 
 // Identify ourself
 define('WT_WEBTREES', 'webtrees');
-define('WT_VERSION', '2.0.0-dev');
+define('WT_VERSION', '1.8.0-dev');
 
 // External URLs
 define('WT_WEBTREES_URL', 'https://www.webtrees.net/');
